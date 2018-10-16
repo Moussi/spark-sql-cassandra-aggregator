5fdb2897407bd732b352662b07ce255e354d96e5
Aymen MOUSSI <aymen.moussi.ext@mappy.com>
2018-10-16 14:44:44 +0200

    spark sql rankfunctions

---
+++ src/main/scala/dataframes/DFRankingFunctionsManip.scala
@@ -0,0 +1,30 @@
+package dataframes
+
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object DFRankingFunctionsManip extends App {
+
+
+  val ss = SparkSession.builder().master("local[*]").getOrCreate()
+  import org.apache.spark.sql.functions._
+  import ss.implicits._
+  import utils.StringUtils._
+
+  /***
+    * Ranking Functions
+    */
+  val employersDF = ss.read.csv("src/main/resources/windowmanip.csv").toDF("name", "dep", "salary")
+
+  //employersDF.show
+
+  val window = Window.partitionBy("dep").orderBy(desc("salary"))
+
+  val rankColumn = rank.over(window)
+
+  employersDF.select(col("name"), col("dep"), col("salary"), rankColumn.as("rank")).where(
+  col("rank").leq(3)).show
+  val denseRankColumn = dense_rank.over(window)
+  employersDF.select(col("name"), col("dep"), col("salary"), denseRankColumn.as("dense_rank")).where(
+    col("dense_rank").leq(3)).show
+}
