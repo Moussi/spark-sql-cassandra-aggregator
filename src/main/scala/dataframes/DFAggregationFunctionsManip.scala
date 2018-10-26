b7b958af5b07926bfeaaa3dd17d37dfffa6f2164
Aymen MOUSSI <aymen.moussi.ext@mappy.com>
2018-10-26 17:12:31 +0200

    spark sql agg

--- src/main/scala/dataframes/DFAggregationFunctionsManip.scala
+++ src/main/scala/dataframes/DFAggregationFunctionsManip.scala
@@ -1,25 +1,38 @@
 package dataframes
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.expressions.Window
 
 object DFAggregationFunctionsManip extends App {
 
 
   val ss = SparkSession.builder().master("local[*]").getOrCreate()
+  import ss.implicits._
+  import utils.StringUtils._
   import org.apache.spark.sql.functions._
 
     /***
     * Ranking Functions
     */
-  val employersDF = ss.read.csv("src/main/resources/windowmanip.csv").toDF("name", "dep", "salary")
+    val italianPosts = ss.sparkContext.textFile("src/main/resources/italianPosts.csv").map(_.split("~"))
+  val italianPostsDF = italianPosts.map(post => Post(post(0).toIntSafe,
+    post(1).toTimestampSafe,
+    post(2).toLongSafe,
+    post(3),
+    post(4).toIntSafe,
+    post(5).toTimestampSafe,
+    post(6).toIntSafe,
+    post(7),
+    post(8),
+    post(9).toIntSafe,
+    post(10).toLongSafe,
+    post(11).toLongSafe,
+    post(12).toLong
+  )).toDF
 
-  //employersDF.show
-
-  val window = Window.partitionBy("dep").orderBy(asc("salary")).rowsBetween(-1,1)
-
-  val sumColumn = sum(col("salary")).over(window)
-  employersDF.select(col("name"), col("dep"), col("salary"), sumColumn.as("sum")).show
+  val groupedDF = italianPostsDF.groupBy('ownerUserId, 'tags, 'postTypeId).count.orderBy('ownerUserId desc)show(10) // .count return dataframe
+  val groupedDF2 = italianPostsDF.groupBy('ownerUserId)
+      .agg(max('score).as('maxScore), max('lastActivityDate))
+          .orderBy('maxScore desc).show(10) // .count return dataframe
 
 
 
